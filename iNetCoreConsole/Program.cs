15b35c0884f4f342360795e9d26fa5d877bf42e4
iceexx <liuhaox@outlook.com>
2017-12-26 14:20:16 +0800

    创建iNetCoreConsole

---
+++ iNetCoreConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace iNetCoreConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.Write("Press any key to exit: ");
+            Console.ReadKey();
+        }
+    }
+}
